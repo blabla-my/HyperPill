cb100841ba4b83a891e48dd256d53ff88db63bf1
MingyuanLuo <myluo24@m.fudan.edu.cn>
2025-04-02 00:31:52 +0800

    Update breakpoint support for ASAN

--- breakpoints.cc
+++ breakpoints.cc
@@ -134,11 +134,11 @@ void apply_breakpoints_linux() {
             });
     add_breakpoint(sym_to_addr("libasan.so.8", "__asan::ReportGenericError"), [](bxInstruction_c *i) {
             printf("ASAN GENERIC ERROR\n");
-            fuzz_stacktrace();
+            fuzz_emu_stop_crash("asan-generic-error");
             });
     add_breakpoint(sym_to_addr("libasan.so.8", "__asan::AsanOnDeadlySignal"), [](bxInstruction_c *i) {
             printf("ASAN Deadly Signal\n");
-            fuzz_stacktrace();
+            fuzz_emu_stop_crash("asan-deadly-signal");
             });
     add_breakpoint(sym_to_addr("vmm", "__stdio_write"), bp__stdio_write);
     add_breakpoint(sym_to_addr("ld-musl", "__stdio_write"), bp__stdio_write);
