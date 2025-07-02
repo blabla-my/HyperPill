86c1013503e851ffa6eb48618f671f36337d06e1
MingyuanLuo <myluo24@m.fudan.edu.cn>
2025-07-02 15:21:47 +0800

    Add class TaskManager for task finding and management. Also, add code for fetching current task. For kernel mode, this can be done by accessing percpu variable current_task. For user mode, this can be done by indexing user task with CR3.

--- task.cc
+++ task.cc
@@ -1,14 +1,21 @@
 #include "task.h"
+#include "bochs.h"
 #include "config.h"
+#include "cpu/cpu.h"
+#include "cpu/decoder/decoder.h"
 #include "cpu/tlb.h"
+#include "fuzz.h"
 #include <cstddef>
 #include <cstdint>
 #include <cstdio>
+#include <cstdlib>
 #include <string>
 #include <sys/types.h>
 
-tsl::robin_map<unsigned long, struct task*> task_map;
+tsl::robin_map<unsigned long, struct task*> cr3_task_map;
 tsl::robin_set<struct task*> hypervisor_tasks;
+
+TaskManager task_manager;
 
 static std::string hypervisor_task_signatures[] = {
     "qemu-system",
@@ -44,14 +51,20 @@ int read_mm_struct(bx_address mm_struct, void* buf, size_t len){
     return 0; // Success
 }
 
-int task_buf_to_fuzz_task(const uint8_t* task_buf, struct task* fuzz_task_ptr) {
-    if (!task_buf || !fuzz_task_ptr) {
+int task_buf_to_task(const uint8_t* task_buf, task* task_ptr) {
+    if (!task_buf || !task_ptr) {
         return -1; // Invalid buffer
     }
-    memset(fuzz_task_ptr, 0, sizeof(struct task)); // Clear the struct
-    fuzz_task_ptr->pid = task_pid(task_buf);
-    fuzz_task_ptr->kernel_task = (task_mm(task_buf) == 0);
-    memcpy(fuzz_task_ptr->comm, task_comm(task_buf), sizeof(fuzz_task_ptr->comm));
+    memset(task_ptr, 0, sizeof(struct task)); // Clear the struct
+    memcpy(task_ptr->comm, task_comm(task_buf), sizeof(task_ptr->comm));
+
+    task_ptr->pid = task_pid(task_buf);
+    // task_ptr->kernel_task = (task_mm(task_buf) == 0);
+    task_ptr->next = task_next(task_buf);
+    task_ptr->prev = task_prev(task_buf);
+    task_ptr->flags = task_flags(task_buf);
+    task_ptr->kernel_task = !!(task_ptr->flags & (PF_KTHREAD|PF_VCPU));
+    task_ptr->stack = task_stack(task_buf);
 
     unsigned long mm = task_mm(task_buf);
     if (mm == 0) {
@@ -59,8 +72,8 @@ int task_buf_to_fuzz_task(const uint8_t* task_buf, struct task* fuzz_task_ptr) {
     }
 
     if (mm == 0) {
-        fuzz_task_ptr->pgd = 0; // Kernel threads do not have a user space memory
-        fuzz_task_ptr->cr3 = 0; // Kernel threads do not have a user space memory
+        task_ptr->pgd = 0; // Kernel threads do not have a user space memory
+        task_ptr->cr3 = 0; // Kernel threads do not have a user space memory
     }
     else {
         // fuzz_task_ptr->pgd = task_mm(task_buf);
@@ -69,21 +82,21 @@ int task_buf_to_fuzz_task(const uint8_t* task_buf, struct task* fuzz_task_ptr) {
         if (read_mm_struct(mm, mm_buf, sizeof(mm_buf)) < 0) {
             return -1; // Failed to read mm_struct
         }
-        fuzz_task_ptr->pgd = mm_pgd(mm_buf);
-        fuzz_task_ptr->cr3 = pgd2cr3(fuzz_task_ptr->pgd);
+        task_ptr->pgd = mm_pgd(mm_buf);
+        task_ptr->cr3 = pgd2cr3(task_ptr->pgd);
     }
 
     // check if task->comm contains a hypervisor signature
-    std::string comm_str(fuzz_task_ptr->comm);
+    std::string comm_str(task_ptr->comm);
     for (const auto& signature : hypervisor_task_signatures) {
         if (comm_str.find(signature) != std::string::npos) {
-            fuzz_task_ptr->hypervisor_task = 1;
+            task_ptr->hypervisor_task = 1;
             break;
         }
     }
     for (const auto& signature : userspace_vmm_task_signatures) {
         if (comm_str.find(signature) != std::string::npos) {
-            fuzz_task_ptr->userspace_vmm_task = 1;
+            task_ptr->userspace_vmm_task = 1;
             break;
         }
     }
@@ -97,32 +110,15 @@ unsigned long pgd2cr3(unsigned long pgd) {
 }
 
 void iterate_tasks(bx_address task_struct_head) {
-    uint8_t task_buf[TASK_SIZE];
-    uint8_t mm_buf[MM_SIZE];
     bx_address task = task_struct_head;
-    bx_address mm;
     do {
-        struct task* fuzz_task_ptr = (struct task*)malloc(sizeof(struct task));
-        if (read_task_struct(task, task_buf, sizeof(task_buf)) < 0) {
-            printf("Failed to read task_struct at %lx\n", task);
-            return;
-        }
-        if (task_buf_to_fuzz_task(task_buf, fuzz_task_ptr) < 0) {
-            printf("Failed to convert task buffer to fuzz_task\n");
-            return;
-        }
+        struct task* task_ptr = task_manager.add_task(task);
 
-        task_map[fuzz_task_ptr->cr3 >> PAGE_SHIFT] = fuzz_task_ptr;
-        
-        if (fuzz_task_ptr->hypervisor_task){
-            hypervisor_tasks.insert(fuzz_task_ptr);
-        }
+        printf("Task at %lx PID: %d, Kernel Thread: %d, Hypervisor Thread: %d, Userspace VMM: %d, Comm: %s, CR3: %lx, PGD: %lx, flags: %x, stack: %lx\n",
+               task, task_ptr->pid, task_ptr->kernel_task, task_ptr->hypervisor_task, task_ptr->userspace_vmm_task, task_ptr->comm,
+               task_ptr->cr3, task_ptr->pgd, task_ptr->flags, task_ptr->stack);
 
-        printf("Task PID: %d, Kernel Thread: %d, Hypervisor Thread: %d, Userspace VMM: %d, Comm: %s, CR3: %lx, PGD: %lx\n",
-               fuzz_task_ptr->pid, fuzz_task_ptr->kernel_task, fuzz_task_ptr->hypervisor_task, fuzz_task_ptr->userspace_vmm_task, fuzz_task_ptr->comm,
-               fuzz_task_ptr->cr3, fuzz_task_ptr->pgd);
-
-        task = task_next(task_buf);
+        task = task_ptr->next;
     } while (task != task_struct_head);
 }
 
@@ -137,6 +133,11 @@ bool is_hypervisor_task(unsigned long cr3) {
     return true;
 }
 
+bool is_hypervisor_task(task* task) {
+    if (!task) return false;
+    return task->hypervisor_task;
+}
+
 bool is_userspace_vmm_task(unsigned long cr3) {
     auto* task = get_task_by_cr3(cr3);
     if (task == NULL)
@@ -144,17 +145,156 @@ bool is_userspace_vmm_task(unsigned long cr3) {
     return task->userspace_vmm_task;
 }
 
+bool is_userspace_vmm_task(task* task){
+    if (!task) return false;
+    return task->userspace_vmm_task;
+}
+
 bool set_hypervisor_task_by_cr3(unsigned long cr3) {
-    if (task_map.find(cr3>>PAGE_SHIFT) == task_map.end()) {
+    if (cr3_task_map.find(cr3>>PAGE_SHIFT) == cr3_task_map.end()) {
         return false;
     }
-    task_map[cr3>>PAGE_SHIFT]->hypervisor_task = true;
+    cr3_task_map[cr3>>PAGE_SHIFT]->hypervisor_task = true;
     return true;
 }
 
 struct task* get_task_by_cr3(unsigned long cr3) {
-    if (task_map.find(cr3>>PAGE_SHIFT) == task_map.end()) {
+    if (cr3_task_map.find(cr3>>PAGE_SHIFT) == cr3_task_map.end()) {
         return NULL;
     }
-    return task_map[cr3>>PAGE_SHIFT];
+    return cr3_task_map[cr3>>PAGE_SHIFT];
+}
+
+TaskManager::TaskManager(){
+    // current_task = sym_to_addr("vmlinux", "current_task");
+    current_task = 0;
+}
+
+TaskManager::~TaskManager(){
+    return;
+}
+
+task* TaskManager::get_task(bx_address task_addr){
+    if (!task_addr) return NULL;
+    if (task_map.contains(task_addr)){
+        return task_map[task_addr];
+    }
+    return NULL;
+}
+
+bx_address TaskManager::get_current_task_bx_addr(){
+    if (!current_task) {
+        current_task = sym_to_addr("vmlinux", "current_task");
+        printf("TaskManager: set current_task: %lx\n", current_task);
+    }
+
+    bx_address taskpp = BX_CPU(id)->get_laddr(BX_SEG_REG_GS, current_task);
+    // check whether GS==0
+    if (taskpp == current_task) return 0;
+
+    bx_address taskp;
+    if (BX_CPU(id)->access_read_linear(taskpp, sizeof(bx_address), 0, BX_READ, 0, &taskp) < 0){
+        printf("Error: failed to read task pointer at current_task %lx\n", taskpp);
+        return 0;
+    }
+    return taskp;
+}
+
+task* TaskManager::get_current_task(){
+    // only in kernel mode, GS is not 0
+    if (BX_CPU(id)->get_cpl() == 0){
+        bx_address current_task_bx_addr = get_current_task_bx_addr();
+        if (!current_task_bx_addr) return NULL;
+        task* current_task_fuzz = get_task(current_task_bx_addr);
+        if (!current_task_fuzz) {
+            return add_task(current_task_bx_addr);
+        } else {
+            return current_task_fuzz;
+        }
+    }
+    else {
+        if (user_task_map.contains(BX_CPU(id)->cr3 >> PAGE_SHIFT))
+            return user_task_map[BX_CPU(id)->cr3 >> PAGE_SHIFT];
+        else
+            return NULL;
+    }
+}
+
+task* TaskManager::get_hypervisor_task(bx_address task_addr){
+    if (hypervisor_task_map.contains(task_addr)){
+        return hypervisor_task_map.at(task_addr);
+    }
+    return NULL;
+}
+
+task* TaskManager::add_task(bx_address task_addr){
+    task* already_in = get_task(task_addr);
+    if (!already_in){
+        task* new_task = alloca_task(task_addr);
+        if (!new_task) return NULL;
+        if (!new_task->kernel_task){
+            // if new_task is a userspace task, index it by CR3 as well
+            bx_address index = new_task->cr3 >> PAGE_SHIFT;
+            user_task_map[index] = new_task;
+            printf("add_task: index task %s by %lx, flags %x\n", new_task->comm, index, new_task->flags);
+        } 
+        task_map[task_addr] = new_task;
+        return new_task;
+    }
+    return already_in;
+}
+
+task* TaskManager::add_task(task* task_addr){
+    // assume task_addr->vaddr != 0
+    if (!task_addr->vaddr){
+        return NULL;
+    }
+    if (has_task(task_addr->vaddr)){
+        return get_task(task_addr->vaddr);
+    }
+    task_map[task_addr->vaddr] = task_addr;
+    return task_addr;
+}
+
+task* TaskManager::add_hypervisor_task(bx_address task_addr){
+    task* already_in = get_hypervisor_task(task_addr);
+    if (!already_in){
+        task* new_task = add_task(task_addr);
+        hypervisor_task_map[task_addr] = new_task;
+        return new_task;
+    }
+    return already_in;
+}
+
+bool TaskManager::has_task(bx_address task_addr){
+    return task_map.contains(task_addr);
+}
+
+bool TaskManager::del_task(bx_address task_addr){
+    // Not implemeneted yet
+    return true;
+}
+
+bool TaskManager::has_hypervisor_task(bx_address task_addr){
+    return hypervisor_task_map.contains(task_addr);
+}
+
+task* TaskManager::alloca_task(bx_address task_addr){
+    uint8_t *task_buf = (uint8_t*)malloc(TASK_SIZE);
+    if (!task_buf){
+        printf("Error: failed to allocate task_buf\n");
+        return NULL;
+    };
+    if (read_task_struct(task_addr, task_buf, TASK_SIZE) < 0){
+        printf("Error: failed to read task struct from guest memory\n");
+        free(task_buf);
+        return NULL;
+    }
+    task* new_task = (task*)malloc(sizeof(task));
+    if (task_buf_to_task(task_buf, new_task) < 0){
+        printf("Error: failed to convert task buf to task\n");
+        return NULL;
+    }
+    new_task->vaddr = task_addr;
+    return new_task;
 }
--- task.h
+++ task.h
@@ -17,9 +17,17 @@ struct task {
     char comm[16];       /* Process name */
     unsigned long cr3;  /* CR3 register */
     unsigned long pgd;  /* Page Global Directory */
+
+    bx_address vaddr;   /* Kernel space address of the task */
+    bx_address next;
+    bx_address prev;
+
+    bx_address stack;
+
+    unsigned int flags;
 };
 
-extern tsl::robin_map<unsigned long, struct task*> task_map;
+extern tsl::robin_map<unsigned long, struct task*> cr3_task_map;
 extern tsl::robin_set<struct task*> hypervisor_tasks;
 
 extern bx_phy_address current_task;
@@ -30,13 +38,15 @@ bx_address get_current_task_addr();
 /* functions to read structures from bx VM */
 int read_task_struct(bx_address task_struct, void* buf, size_t len);
 int read_mm_struct(bx_address mm_struct, void* buf, size_t len);
-int task_buf_to_fuzz_task(const uint8_t* task_buf, task& fuzz_task);
+int task_buf_to_task(const uint8_t* task_buf, task* task);
 unsigned long pgd2cr3(unsigned long pgd);
 
 void iterate_tasks(bx_address task_struct_head);
 
 bool is_hypervisor_task(unsigned long cr3);
+bool is_hypervisor_task(task* task);
 bool is_userspace_vmm_task(unsigned long cr3);
+bool is_userspace_vmm_task(task* task);
 
 bool set_hypervisor_task_by_cr3(unsigned long cr3);
 struct task* get_task_by_cr3(unsigned long cr3);
@@ -44,6 +54,46 @@ struct task* get_task_by_cr3(unsigned long cr3);
 /* macros for operating struct task_struct */
 /* kernel version 6.0.32 */
 #define PAGE_SHIFT 12
+
+/* copied from linx/sched.h */
+/*
+ * Per process flags
+ */
+#define PF_KTHREAD		0x00200000	/* I am a kernel thread */
+#define PF_VCPU			0x00000001	/* I'm a virtual CPU */
+#define PF_IDLE			0x00000002	/* I am an IDLE thread */
+#define PF_EXITING		0x00000004	/* Getting shut down */
+#define PF_POSTCOREDUMP		0x00000008	/* Coredumps should ignore this task */
+#define PF_IO_WORKER		0x00000010	/* Task is an IO worker */
+#define PF_WQ_WORKER		0x00000020	/* I'm a workqueue worker */
+#define PF_FORKNOEXEC		0x00000040	/* Forked but didn't exec */
+#define PF_MCE_PROCESS		0x00000080      /* Process policy on mce errors */
+#define PF_SUPERPRIV		0x00000100	/* Used super-user privileges */
+#define PF_DUMPCORE		0x00000200	/* Dumped core */
+#define PF_SIGNALED		0x00000400	/* Killed by a signal */
+#define PF_MEMALLOC		0x00000800	/* Allocating memory to free memory. See memalloc_noreclaim_save() */
+#define PF_NPROC_EXCEEDED	0x00001000	/* set_user() noticed that RLIMIT_NPROC was exceeded */
+#define PF_USED_MATH		0x00002000	/* If unset the fpu must be initialized before use */
+#define PF_USER_WORKER		0x00004000	/* Kernel thread cloned from userspace thread */
+#define PF_NOFREEZE		0x00008000	/* This thread should not be frozen */
+#define PF_KCOMPACTD		0x00010000	/* I am kcompactd */
+#define PF_KSWAPD		0x00020000	/* I am kswapd */
+#define PF_MEMALLOC_NOFS	0x00040000	/* All allocations inherit GFP_NOFS. See memalloc_nfs_save() */
+#define PF_MEMALLOC_NOIO	0x00080000	/* All allocations inherit GFP_NOIO. See memalloc_noio_save() */
+#define PF_LOCAL_THROTTLE	0x00100000	/* Throttle writes only against the bdi I write to,
+						 * I am cleaning dirty pages from some other bdi. */
+#define PF_KTHREAD		0x00200000	/* I am a kernel thread */
+#define PF_RANDOMIZE		0x00400000	/* Randomize virtual address space */
+#define PF__HOLE__00800000	0x00800000
+#define PF__HOLE__01000000	0x01000000
+#define PF__HOLE__02000000	0x02000000
+#define PF_NO_SETAFFINITY	0x04000000	/* Userland is not allowed to meddle with cpus_mask */
+#define PF_MCE_EARLY		0x08000000      /* Early kill for mce process policy */
+#define PF_MEMALLOC_PIN		0x10000000	/* Allocations constrained to zones which allow long term pinning.
+						 * See memalloc_pin_save() */
+#define PF_BLOCK_TS		0x20000000	/* plug has ts that needs updating */
+#define PF__HOLE__40000000	0x40000000
+#define PF_SUSPEND_TASK		0x80000000      /* This thread called freeze_processes() and should not be frozen */
 
 #define TASK_SIZE 0x2640
 #define TASK_OFFSET_MM 0x8e0
@@ -53,13 +103,23 @@ struct task* get_task_by_cr3(unsigned long cr3);
 #define TASK_OFFSET_COMM 0xba0
 #define TASK_OFFSET_NEXT (TASK_OFFSET_TASKS + 0x0)
 #define TASK_OFFSET_PREV (TASK_OFFSET_TASKS + sizeof(unsigned long))
+#define TASK_OFFSET_FLAGS 0x2c //unsigned int
+#define TASK_OFFSET_STACK 0x20 //void *
+
+#define task_field(ts,_off,_type) *(_type*)((unsigned long)ts+_off)
 
 #define task_next(ts) (*(unsigned long*)((unsigned long)ts + TASK_OFFSET_NEXT) - TASK_OFFSET_TASKS)
 #define task_prev(ts) (*(unsigned long*)((unsigned long)ts + TASK_OFFSET_PREV) - TASK_OFFSET_TASKS)
-#define task_mm(ts) (*(unsigned long*)((unsigned long)ts + TASK_OFFSET_MM))
-#define task_active_mm(ts) (*(unsigned long*)((unsigned long)ts + TASK_OFFSET_ACTIVE_MM))
-#define task_pid(ts) (*(int*)((unsigned long)ts + TASK_OFFSET_PID))
+// #define task_mm(ts) (*(unsigned long*)((unsigned long)ts + TASK_OFFSET_MM))
+// #define task_active_mm(ts) (*(unsigned long*)((unsigned long)ts + TASK_OFFSET_ACTIVE_MM))
+// #define task_pid(ts) (*(int*)((unsigned long)ts + TASK_OFFSET_PID))
 #define task_comm(ts) ((char*)((unsigned long)ts + TASK_OFFSET_COMM))
+
+#define task_mm(ts) task_field(ts,TASK_OFFSET_MM,unsigned long)
+#define task_active_mm(ts) task_field(ts,TASK_OFFSET_ACTIVE_MM,unsigned long)
+#define task_pid(ts) task_field(ts,TASK_OFFSET_PID,int)
+#define task_flags(ts) task_field(ts,TASK_OFFSET_FLAGS,unsigned int)
+#define task_stack(ts) task_field(ts,TASK_OFFSET_STACK,unsigned long)
 
 /* macros for operating struct mm */
 #define MM_SIZE 0x440
@@ -73,5 +133,34 @@ struct task* get_task_by_cr3(unsigned long cr3);
 
 
 /* Class for task */
+class TaskManager {
+public:
+    TaskManager();
+    ~TaskManager();
+    
+    task* get_task(bx_address task_addr);
+    task* get_current_task();
+    bx_address get_current_task_bx_addr();
+    task* get_hypervisor_task(bx_address task_addr);
+    task* add_task(bx_address task_addr);
+    task* add_task(task* task_addr); //we should never allocate a task out of TaskManager.
+    task* add_hypervisor_task(bx_address task_addr);
+    bool has_task(bx_address task_addr);
+    bool del_task(bx_address task_addr);
+    bool has_hypervisor_task(bx_address task_addr);
+    // bool hypervisor_task(task* task_addr);
+
+private:
+    tsl::robin_map<bx_address, task*> task_map;
+    tsl::robin_map<bx_address, task*> user_task_map;
+    tsl::robin_map<bx_address, task*> hypervisor_task_map;
+    bx_address current_task;
+
+    task* alloca_task(bx_address task_addr);
+    // bool add_hypervisor_task(task* task_addr);
+    // bool has_task(task* task_addr);
+};
+
+extern TaskManager task_manager;
 
 #endif
