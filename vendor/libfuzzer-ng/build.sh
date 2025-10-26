6ecce0d5031592bc1207348881775a60f996af17
MingyuanLuo <myluo24@m.fudan.edu.cn>
2025-10-26 17:53:37 +0800

    update the Makefile to support DEBUG build

--- vendor/libfuzzer-ng/build.sh
+++ vendor/libfuzzer-ng/build.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 LIBFUZZER_SRC_DIR=$(dirname $0)
 CXX="${CXX:-clang}"
+OPT_LEVEL="-O2"
+if [ "$DEBUG" = "1" ]; then
+  OPT_LEVEL="-O0"
+fi
+
 for f in $LIBFUZZER_SRC_DIR/*.cpp; do
-  $CXX -g -O2 -fno-omit-frame-pointer -std=c++11 -fPIE $f -c &
+  $CXX -g $OPT_LEVEL -fno-omit-frame-pointer -std=c++11 -fPIE $f -c &
 done
 wait
 rm -f libFuzzer.a
 ar ru libFuzzer.a Fuzzer*.o
 rm -f Fuzzer*.o
-
