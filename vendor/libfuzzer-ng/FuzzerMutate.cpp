3a9ba309aca11e08823475cfe63c5e8a45ed2c28
MingyuanLuo <myluo24@m.fudan.edu.cn>
2025-12-03 18:59:53 +0800

    do not use Mutate_InsertRepeatedByte on DMAData

--- vendor/libfuzzer-ng/FuzzerMutate.cpp
+++ vendor/libfuzzer-ng/FuzzerMutate.cpp
@@ -472,7 +472,7 @@ size_t MutationDispatcher::Mutate_InsertRepeatedBytesWithType(uint8_t *Data,
   if (type == fuzzer::HotPos::OPS)                                                      
     return Mutate_InsertRepeatedBytes(Data, Size, MaxSize);
   else
-    return Mutate_InsertRepeatedBytes(Data, Size, MaxSize);
+    return 0;
 }
 
 size_t MutationDispatcher::Mutate_ChangeByte(uint8_t *Data, size_t Size,
