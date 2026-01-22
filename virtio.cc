bd43f4b170d7b93bf29f823c08bf93adf1b28356
MingyuanLuo <myluo24@m.fudan.edu.cn>
2026-01-22 21:05:08 +0800

    still use possible switch modeling for non-scsi device

--- virtio.cc
+++ virtio.cc
@@ -1556,7 +1556,6 @@ static int ingest_vring_buffer(unsigned cpu, bx_address addr, bx_address gpa, si
 	size_t offset = queue->desc_chain_fsm.get_request_offset(gpa);
 	bool is_out = desc_with_info->desc_info.is_out;
 	bool possible_switch = (offset == 0 && is_out && desc_with_info->desc_info.desc_idx == 0);
-	possible_switch = false;
 			
 	if (offset > 0x100) 
 		return 0;
