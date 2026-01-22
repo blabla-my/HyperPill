ae54ab32b4bea3db1c8fb4eaa3658cea58363b8a
MingyuanLuo <myluo24@m.fudan.edu.cn>
2026-01-22 17:38:37 +0800

    drain: throttle predicate checks by tick interval

--- main.cc
+++ main.cc
@@ -37,12 +37,15 @@ bool fuzzing;
 static bool executing_input;
 
 static constexpr unsigned long int kDrainIcountBudget = 5000000;
+static constexpr size_t kDrainPredTickInterval = 1000000;
 
 static struct {
 	bool active;
 	drain_predicate_t pred;
 	void* ctx;
 	bool budget_hit;
+	size_t pred_calls;
+	size_t tickn_calls;
 } drain_state = {};
 
 void drain_begin(drain_predicate_t pred, void* ctx) {
@@ -50,6 +53,8 @@ void drain_begin(drain_predicate_t pred, void* ctx) {
 	drain_state.pred = pred;
 	drain_state.ctx = ctx;
 	drain_state.budget_hit = false;
+	drain_state.pred_calls = 0;
+	drain_state.tickn_calls = 0;
 }
 
 DrainStats drain_end() {
@@ -59,6 +64,8 @@ DrainStats drain_end() {
 	drain_state.active = false;
 	drain_state.pred = nullptr;
 	drain_state.ctx = nullptr;
+	drain_state.pred_calls = 0;
+	drain_state.tickn_calls = 0;
 	return stats;
 }
 
@@ -226,8 +233,14 @@ void start_cpu(bool enumerating) {
 				BX_TICKN(executed / bx_cpu_count);
 				executed %= bx_cpu_count;
 				if (drain_state.active) {
-					if (drain_state.pred && drain_state.pred(drain_state.ctx))
-						pause_cpu();
+					drain_state.tickn_calls++;
+					if (drain_state.pred &&
+					    drain_state.tickn_calls %
+						    kDrainPredTickInterval == 0) {
+						drain_state.pred_calls++;
+						if (drain_state.pred(drain_state.ctx))
+							pause_cpu();
+					}
 				}
 			}
 
