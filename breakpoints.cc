622f22b1c540fd44bb34b166436d91196eb0bdec
MingyuanLuo <myluo24@m.fudan.edu.cn>
2026-01-27 20:57:55 +0800

    Handle exit syscall under NOCOV
    - Always treat exit/exit_group syscalls as crash signals, even when NOCOV is set

--- breakpoints.cc
+++ breakpoints.cc
@@ -235,14 +235,13 @@ void handle_syscall_hooks(unsigned cpu, bxInstruction_c *i)
     /* Hook Syscalls */
     static void* nocov = getenv("NOCOV");
     if (i->getIaOpcode() == 0x471) {
-        switch(BX_CPU(cpu)->gen_reg[BX_64BIT_REG_RAX].rrx) {
-            case 231:
-            case 60:    // exit
-                if (nocov)
-                    fuzz_emu_stop_crash(cpu, "exit-syscall");
-                break;
-            case 62:    // kill
-            case 200:   // tkill
+		switch(BX_CPU(cpu)->gen_reg[BX_64BIT_REG_RAX].rrx) {
+			case 231:
+			case 60:    // exit
+				fuzz_emu_stop_crash(cpu, "exit-syscall");
+				break;
+			case 62:    // kill
+			case 200:   // tkill
                 if (nocov && BX_CPU(cpu)->gen_reg[BX_64BIT_REG_RSI].rrx == 6) { // SIGABRT
                     fuzz_emu_stop_crash(cpu, "kill-syscall");
                     return;
